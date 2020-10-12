2bbfafa965ddf2935d84fcf83e79da819d225709
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-10-11 20:51:31 -0400

    created hook in useForm, set state in class component PlantList, mounted and fetched an axios call

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (key, initialValue) => {
+    const [values, setValues] = useState(key, initialValue);
+    const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    return [values, handleChanges];
+};
